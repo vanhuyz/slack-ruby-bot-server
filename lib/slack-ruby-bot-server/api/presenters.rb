562c38d80a4f237e78277b54e535ab110b297f3a
Huy Van <van-huy@cookpad.com>
2020-12-10 19:17:55 +0900

    Remove team presenters

--- lib/slack-ruby-bot-server/api/presenters.rb
+++ lib/slack-ruby-bot-server/api/presenters.rb
@@ -4,6 +4,4 @@ require 'roar/json/hal'
 
 require_relative 'presenters/paginated_presenter'
 require_relative 'presenters/status_presenter'
-require_relative 'presenters/team_presenter'
-require_relative 'presenters/teams_presenter'
 require_relative 'presenters/root_presenter'
